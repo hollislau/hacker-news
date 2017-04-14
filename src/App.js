d987863ad0bc5727895054eb53427eab586e770b
Hollis Lau <hollislau@hotmail.com>
2017-04-13 23:28:17 -0700

    Add conditional to greeting component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const greeting = 'Hello World';
+    const greeting = 'Hello';
+    const user = {
+      firstName: 'Asuna',
+      lastName: 'Yuki'
+    }
+
+    function greetUser(greeting, user) {
+      if (user) {
+        return greeting + ', ' + user.firstName + ' ' + user.lastName + '!';
+      }
+      return greeting + ', stranger!';
+    }
 
     return (
       <div className="App">
-          <h2>{greeting}</h2>
+          <h2>{greetUser(greeting, user)}</h2>
       </div>
     );
   }
