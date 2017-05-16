69282c24b64cabab02629594d27dd60661ba40f1
Hollis Lau <hollislau@hotmail.com>
2017-05-16 01:32:10 -0700

    Add list search functionality

--- src/App.js
+++ src/App.js
@@ -20,12 +20,16 @@ const list = [
   },
 ];
 
+const isSearched = (searchTerm) => (item) =>
+  !searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      list
+      list,
+      searchTerm: ''
     }
   }
 
@@ -36,10 +40,20 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
     return (
       <div className="App">
-        { this.state.list.map((item) =>
+        <form>
+          <input
+            type="text"
+            onChange={ this.onSearchChange }
+            />
+        </form>
+        { this.state.list.filter(isSearched(this.state.searchTerm)).map((item) =>
           <div key={ item.objectID }>
             <span>
               <a href={ item.url }>{ item.title }</a>
