8849d36c2fcca256139d0cfa972105875fcb7a12
Hollis Lau <hollislau@hotmail.com>
2017-05-19 18:54:59 -0700

    Refactor conditional renderings

--- src/App.js
+++ src/App.js
@@ -107,8 +107,6 @@ class App extends Component {
   render() {
     const { result, searchTerm } = this.state;
 
-    if (!result) return null;
-
     return (
       <div className="page">
         <div className="interactions">
@@ -119,11 +117,13 @@ class App extends Component {
             Search
           </Search>
         </div>
-        <Table
-          list={ result.hits }
-          pattern={ searchTerm }
-          onDismiss={ this.onDismiss }
-        />
+        { result &&
+          <Table
+            list={ result.hits }
+            pattern={ searchTerm }
+            onDismiss={ this.onDismiss }
+          />
+        }
       </div>
     );
   }
