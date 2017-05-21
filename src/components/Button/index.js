843b00df14ecbd5684d6b56062172f8430a5e3c0
Hollis Lau <hollislau@hotmail.com>
2017-05-20 18:44:37 -0700

    Add type checking for props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 
-const Button = ({ onClick, className = '', children }) =>
+const Button = ({ onClick, className, children }) =>
   <button
     onClick={ onClick }
     className={ className }
@@ -9,5 +10,15 @@ const Button = ({ onClick, className = '', children }) =>
   >
     { children }
   </button>
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
+
+Button.defaultProps = {
+  className: ''
+}
 
 export default Button;
