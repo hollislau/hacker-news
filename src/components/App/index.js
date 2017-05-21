f3027b28620ab4106f169e3e8a1b834197e74072
Hollis Lau <hollislau@hotmail.com>
2017-05-20 17:56:56 -0700

    Refactor components into modules

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,70 +1,18 @@
 import React, { Component } from 'react';
+import Button from '../Button';
+import Search from '../Search';
+import Table from '../Table';
+import {
+  DEFAULT_QUERY,
+  DEFAULT_PAGE,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from '../../constants';
 import './index.css';
-
-const DEFAULT_QUERY = 'Redux';
-const DEFAULT_PAGE = '0';
-const DEFAULT_HPP = '50';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
-
-const largeColumn = { width: '40%' };
-const midColumn = { width: '30%' };
-const smallColumn = { width: '10%' };
-
-const Button = ({ onClick, className = '', children }) =>
-  <button
-    onClick={ onClick }
-    className={ className }
-    type="button"
-  >
-    { children }
-  </button>
-
-const Search = ({ value, onChange, onSubmit, children }) =>
-  <form onSubmit={ onSubmit }>
-    <input
-      type="text"
-      value={ value }
-      onChange={ onChange }
-    />
-    <button type="submit">
-      { children }
-    </button>
-  </form>
-
-const Table = ({ list, onDismiss }) =>
-  <div className="table">
-    { list.map((item) =>
-      <div
-        key={ item.objectID }
-        className="table-row"
-      >
-        <span style={ largeColumn }>
-          <a
-            href={ item.url }
-            target="_blank"
-          >
-            { item.title }
-          </a>
-        </span>
-        <span style={ midColumn }>{ item.author }</span>
-        <span style={ smallColumn }>{ item.num_comments }</span>
-        <span style={ smallColumn }>{ item.points }</span>
-        <span style={ smallColumn}>
-          <Button
-            onClick={ () => onDismiss(item.objectID) }
-            className="button-inline"
-          >
-            Dismiss
-          </Button>
-        </span>
-      </div>
-    )}
-  </div>
 
 class App extends Component {
   constructor(props) {
@@ -173,4 +121,4 @@ class App extends Component {
   }
 }
 
-export default App;//
+export default App;
